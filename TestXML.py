e67c5afe8ed66c20339efb64b363e6bf982600ff
Hoang Pham <hphamnk@yahoo.com>
2013-06-20 05:40:59 -0500

    Initial Commit

---
+++ TestXML.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# -------------------------------
+# projects/collatz/TestCollatz.py
+# Copyright (C) 2013
+# Glenn P. Downing
+# -------------------------------
+
+"""
+To test the program:
+    % python TestCollatz.py >& TestCollatz.py.out
+    % chmod ugo+x TestCollatz.py
+    % TestCollatz.py >& TestCollatz.py.out
+"""
+
+# -------
+# imports
+# -------
+
+import StringIO
+import unittest
+
+# replace with XML stuff
+# from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve, cycle_length
+
+# -----------
+# TestXML
+# -----------
+
+class TestXML (unittest.TestCase) :
+    # ----
+    # 
+    # ----
+
+
+
+# ----
+# main
+# ----
+
+print ("TestXML.py")
+unittest.main()
+print ("Done.")
