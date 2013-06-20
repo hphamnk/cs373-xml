6dec274106f804447efc74c9b0e48c9f2be75362
Hoang Pham <hphamnk@yahoo.com>
2013-06-20 14:14:03 -0500

    Done with numbering
    split xml and list
    flexible with list

--- XML.py
+++ XML.py
@@ -6,32 +6,40 @@ import xml.etree.ElementTree as ET
 # -------------
 
 def xml_solve (r, w) :
-    """
-    r is a reader
-    w is a writer
-    """
-    querry = ["Team", "Cooly"]
+	"""
+	r is a reader
+	w is a writer
+	"""
+	querry = ["Team", "Cooly"]
 
-    tree = ET.parse(r)
-    root = tree.getroot()
-    print "root: ", root
-    print "root.tag: ", root.tag
-    
-    print "\nchildren + grandchildren: "
-    for x in root.iter():
-    	print x.tag
+	tree = ET.parse(r)
+	root = tree.getroot()
+	print "root: ", root
+	print "root.tag: ", root.tag
+	
+	print "\nchildren + grandchildren: "
+	for x in root.iter():
+		print x.tag
 
-    print "\nchildren of root (THU): "
-    for elem in root.getchildren():
-    	print elem.tag
+	print "\nchildren of root (THU): "
+	for elem in root.getchildren():
+		print elem.tag
 
-    print "\nfind tag in children but not grandchildren"
-    for elem in root.findall(querry[0]):
-    	print elem.tag
+	print "\nfind tag in children but not grandchildren"
+	for elem in root.findall(querry[0]):
+		print elem.tag
 
-    print "\nfind querry 0 and 1"
-    for x in root.iter():
-    	#print x.tag
-    	if x.tag == querry[0]:
-    		for elem in x.findall(querry[1]):
-    			print elem.tag
+	print "\nfind querry 0 and 1"
+	countOccur = 0
+	countNumber = 0
+	for x in root.iter():
+		#print x.tag
+		countNumber += 1
+		if x.tag == querry[0]:
+			for elem in x.findall(querry[1]):
+				countOccur += 1
+				print elem.tag
+				print "counterNumber: ", countNumber
+
+
+	print countOccur
